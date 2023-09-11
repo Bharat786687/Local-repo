c7ffd8c13eb13b81320ac5ea2cf8699c74cf03f0
BharatJaidiya <bharatjaidiya30@gmail.com>
2023-09-11 06:06:38 +0530

    fix

--- localrepo.cpp
+++ localrepo.cpp
@@ -4,8 +4,4 @@ using namespace std;
 
 int main () {
     cout << "Bharat" << endl;
-
-    cout << "This is a 90 days challenge" << endl;
-    
-   cout << "i'm on the feature branch" << endl;
-   }
+cout << "i'm on feature branch" << endl;   }
